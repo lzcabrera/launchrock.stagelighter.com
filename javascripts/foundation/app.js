ecd8bbfa3758360b84daab0521dc1b9572ffa05e
Laura Cabrera <lzcabrera@gmail.com>
2012-10-30 15:46:48 -0700

    video image setup

--- javascripts/foundation/app.js
+++ javascripts/foundation/app.js
@@ -31,6 +31,84 @@
     });
   }
  
+
+
+  var tag = document.createElement('script');
+
+  tag.src = "//www.youtube.com/iframe_api";
+
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  var player;
+
+
+
+
+  function loadYouTubeIframeVideo() {    
+     player = new YT.Player('player', {
+          width: '560',
+          height: '315',
+          videoId: 'cP3eRHyPMjc',
+          playerVars: { 
+            'modestbranding': 1,
+            'enablejsapi': 1,
+            'rel': 0,
+            'origin': 'www.stagelighter.com',
+            'showinfo': 0,
+            'controls': 2,
+            'autohide': 1,
+            'theme':'dark'
+          },
+          events: {
+            'onReady': onPlayerReady
+          }
+        }); 
+  }
+
+
+      
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+  $(window).load(function() {
+
+
+    /*
+     var parentHost = undefined;
+
+      try {
+        parentHost = top.location.host;
+      }
+      catch (e) {
+        alert('test catch');
+        // iframe's parent is on a different host - for good browsers
+      }
+
+      if (parentHost == undefined) {
+        alert('test undef');
+        // iframe's parent is on a different host - for bad browsers
+      }
+      else {
+        alert('test same');
+        // iframe's parent is on the same host
+      }
+    */
+
+
+    
+    //function onYouTubeIframeAPIReady() {
+      
+      $('#player').click(function() {
+          loadYouTubeIframeVideo();
+        }); 
+    //}
+
+  });
+
+
 /*
   $(window).load(function() {
        $('#featured1').orbit({ fluid: '3x4',bullets: true,timer: false, directionalNav: true });
