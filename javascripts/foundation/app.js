4a5b9ed995ab493036452cabe1077277ecb25b15
Laura Cabrera <lzcabrera@gmail.com>
2012-10-30 16:29:53 -0700

    touch js update

--- javascripts/foundation/app.js
+++ javascripts/foundation/app.js
@@ -97,7 +97,7 @@
       }
     */
 
-      $('.touch #player .img').css({ opacity: 1 });
+      $('.touch #player .play').css({ opacity: 1 });
     
     //function onYouTubeIframeAPIReady() {
       
